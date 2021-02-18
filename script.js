18d9132368ad21ef1947fdab6593680f4ba6a3d4
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2021-02-18 16:43:25 -0300

    Adding todo menu and function that adds this todo

---
+++ script.js
@@ -0,0 +1,40 @@
+const input_text = document.querySelector(".l-header__text")
+
+input_text.addEventListener("keyup", function(event) {
+    if (event.keyCode == 13) { // enter pressionado
+        const text = input_text.value
+        if (text == 0) {
+            // ALERTA PEDINDO PARA PREENCHER
+            console.log("VAZIO")
+        }
+        else {
+            console.log("NAO ESTA VAZIO")
+            const list = document.querySelector(".c-list")
+            const li = document.createElement("li")
+
+            const todo = createTodo(text)
+            li.innerHTML = todo
+
+            list.appendChild(li)
+            
+            var div = document.createElement("div")
+
+            input_text.value = ''
+            input_text.focus()
+        }
+        // verifico se esta vazio
+
+    }
+})
+
+function createTodo(todo_text) {
+    return (
+        `      
+        <div class="c-list__item">
+        <input type="checkbox" class="c-list__mark" name="mark">
+        <label for="mark"></label>
+        <p>${todo_text}</p>
+        </div>
+      `
+    )
+} 
